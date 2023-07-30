8ffc020f3655ebd3b31ad055c4bf5f9dee4a1b94
Laxmi <laxmibist009@gmail.com>
2023-07-30 13:51:52 +0545

    WCC-5-Implementation-of-category-card-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.scss";
+import Categorysection from "./layouts/Categorysection";
 
 
 function App() {
   return (
     <>
       <p>whole</p>
+      <Categorysection/>
     </>
   );
 }
