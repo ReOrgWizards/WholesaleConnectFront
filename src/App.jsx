d80fbb8bbe38ae5b111bfb7da6b6a08875844d74
Laxmi_Bist <98207003+LaxmiBist@users.noreply.github.com>
2023-08-01 22:58:44 +0545

    WCC-5-Implementation-of-category-card-component (#6)
    * WCC-5-Implementation-of-category-card-component
    
    * WCC-5-implementation-of-Category-Card-Component-update
    
    * WCC-5-Implementatin-of-Cateogory-Card-Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.scss";
+import WCCategoryCard from "./components/cards/categoryCard/categoryCard";
+import { books } from "./assets/Images";
+
 
 
 function App() {
   return (
     <>
-      <p>whole</p>
+   <WCCategoryCard image={books} title="Books"/>
     </>
   );
 }
---
+++ src/assets/Images/index.js
@@ -0,0 +1,7 @@
+import books from "./books.webp";
+import card from "./card.webp";
+import earphone from "./earphone.webp";
+import mobileCase from "./mobileCase.webp";
+import Teacoffee from "./Teacoffee.webp";
+
+export { books, card, earphone, mobileCase, Teacoffee };
