499d6f43ac89402113ed7e61462eda53e68761d7
Laxmi <laxmibist009@gmail.com>
2023-08-02 17:11:43 +0545

    WCC-6-Implementation-of-product-Card

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import "./App.scss";
 import WCCategoryCard from "./components/cards/categoryCard/categoryCard";
 import { books } from "./assets/Images";
 import WCTitle from "./components/TItle/TItle";
-
+import WCProductCard from "./components/cards/productCard/productCard";
 
 function App() {
   return (
     <>
-   <WCCategoryCard image={books} title="Books"/>
-   <WCTitle title="Category Section" />
+      <WCCategoryCard image={books} title="Books" />
+      <WCTitle title="Category Section" />
+      <WCProductCard
+        image={books}
+        title="Cards & Plays"
+        description="This is Card & Plays that every one must play."
+      />
     </>
   );
 }
--- src/components/cards/productCard/productCard.jsx
+++ src/components/cards/productCard/productCard.jsx
@@ -1,8 +1,28 @@
 import React from "react";
-
-const WCProductCard = () => {
-  return
-  <p>product card</p>
+import { CartIcon, FavoriteIcon } from "../../../assets/Icons";
+const WCProductCard = ({ image, title, description }) => {
+  return (
+    <>
+      <div className="product__card__container">
+        <div className="product__card__img__container">
+          <img src={image} alt={title} />
+        </div>
+        <div className="product__card__content">
+          <p className="product__card__title ">{title}</p>
+          <p className="product__card__description">{description}</p>
+          <div className="product__price__icon__container">
+            <p>
+              <span>Rs. </span>1200/-
+            </p>
+            <p className="icon">
+              <CartIcon />
+              <FavoriteIcon />
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default WCProductCard;
