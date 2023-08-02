29c52e59e48c2b9c5456fc3fb77fff378045c5e4
Hypekage <jmestmnag@gmail.com>
2023-08-02 09:16:53 +0545

    WCTitle component tested
    WCTitle was tested with title property and it goes well.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.scss";
 import WCCategoryCard from "./components/cards/categoryCard/categoryCard";
 import { books } from "./assets/Images";
+import WCTitle from "./components/TItle/TItle";
 
 
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
    <WCCategoryCard image={books} title="Books"/>
+   <WCTitle title="Category Section" />
     </>
   );
 }
