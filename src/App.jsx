6e822ed2e6c559e7d2505761cf1b88c9d2f17e25
Laxmi <laxmibist009@gmail.com>
2023-08-03 15:46:22 +0545

    app.js file update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import "./App.scss";
 import WCCategoryCard from "./components/cards/categoryCard/categoryCard";
 import WCTitle from "./components/TItle/TItle";
-// import WCSliderProvider from "./components/slider/sliderProvider";
-import WCProductCard from "./components/cards/productCard/productCard";
+
 
 function App() {
   return (
     <>
       <WCTitle title="Category Section" />
       <WCCategoryCard/>
-      {/* <WCSliderProvider /> */}
-      <WCCategoryCard image={books} title="Books" />
-      <WCTitle title="Category Section" />
-      <WCProductCard
-        image={books}
-        title="Cards & Plays"
-        description="This is Card & Plays that every one must play."
-        price="1200 /-"
-      />
+      
     </>
   );
 }
