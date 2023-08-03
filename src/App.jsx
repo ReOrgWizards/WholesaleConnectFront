7419d5c01b9d2b894196e61499a61b92bd215022
Laxmi_Bist <98207003+LaxmiBist@users.noreply.github.com>
2023-08-03 14:58:49 +0545

    Wcc 6 implementation of product card (#8)
    * blank
    
    * WCC-6-Implementation-of-product-Card
    
    * WCC-6-Implementation-of-Product-card-issues-are-resolved
    
    * issue-resolve-realted-resolution-wcc-6-implementaion-of-product-card

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import "./App.scss";
 import WCCategoryCard from "./components/cards/categoryCard/categoryCard";
 import { books } from "./assets/Images";
 import WCTitle from "./components/TItle/TItle";
-
-
+import WCProductCard from "./components/cards/productCard/productCard";
 
 function App() {
   return (
     <>
-   <WCCategoryCard image={books} title="Books"/>
-   <WCTitle title="Category Section" />
+      <WCCategoryCard image={books} title="Books" />
+      <WCTitle title="Category Section" />
+      <WCProductCard
+        image={books}
+        title="Cards & Plays"
+        description="This is Card & Plays that every one must play."
+        price="1200 /-"
+      />
     </>
   );
 }
