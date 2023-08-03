9245ab0b37202bd8721179e6d576baa94beed108
Laxmi <laxmibist009@gmail.com>
2023-08-03 10:53:01 +0545

    working on WCC-8_implementaion-of-Slider-Section-feature

--- src/components/slider/slickSlider.jsx
+++ src/components/slider/slickSlider.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Slider from "react-slick";
+import SliderProvider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import WCCategoryCard from "../cards/categoryCard/categoryCard";
@@ -69,14 +69,14 @@ const WCSlider = () => {
   return (
     <>
       <div className="slider_container">
-        <Slider {...settings}>
+        <SliderProvider {...settings}>
           <WCCategoryCard image="https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" />
           <WCCategoryCard image="https://plus.unsplash.com/premium_photo-1674228288323-3bfbae70ad45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" />
           <WCCategoryCard image="https://images.unsplash.com/photo-1682687982298-c7514a167088?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" />
           <WCCategoryCard image="https://images.unsplash.com/photo-1682685797527-63b4e495938f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzNnx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
           <WCCategoryCard image="https://images.unsplash.com/photo-1690828850555-bd808ba5691a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" />
           <WCCategoryCard image="https://images.unsplash.com/photo-1682687982185-531d09ec56fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" />
-        </Slider>
+        </SliderProvider>
       </div>
     </>
   );
