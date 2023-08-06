0979335d58b79914d53d37742beb911f0296b9a2
Laxmi <laxmibist009@gmail.com>
2023-08-06 12:19:52 +0545

    WCC-21-Redesign-Colloboration-section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.scss";
-import WCCategoryCard from "./components/cards/categoryCard/categoryCard";
-import WCTitle from "./components/TItle/TItle";
-
+import WCColloborationSection from "./layouts/colloboration/colloboration";
+// import WCCategoryCard from "./components/cards/categoryCard/categoryCard";
+// import WCTitle from "./components/TItle/TItle";
+import { categoryArr } from "./data";
 
 function App() {
   return (
     <>
-      <WCTitle title="Category Section" />
-      <WCCategoryCard/>
-      
+      {/* <WCTitle title="Category Section" />
+      <WCCategoryCard/> */}
+      <WCColloborationSection categoryArr={categoryArr} />
     </>
   );
 }
--- src/data/index.js
+++ src/data/index.js
@@ -1,35 +1,70 @@
+// Categorycont data
 
-// Categorycont data 
-
-export const categoryArr = [{
+export const categoryArr = [
+  {
     id: 1,
     img: "https://plus.unsplash.com/premium_photo-1674228288323-3bfbae70ad45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
     title: "Books",
-    description:"this is book"
-},
-{
+    description: "this is book",
+    brand_img:
+      "https://www.virginmegastore.qa/medias/Capslab-logo.webp?context=bWFzdGVyfHJvb3R8MzU0MnxhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW18aGExL2g1OC8xMDI5OTczMDQ5MzQ3MC9DYXBzbGFiLWxvZ28ud2VicHw0NjYzYjg3Mjg0ZDQ4ODEzMGQ5NGE3YjhhNTRkOWMxNGMwODdlNWYzNTZkNzVkZjk2MjNkZjkzZDBkYjRlYTY5",
+      brand_name:"unsplace9"
+  },
+  {
     id: 2,
-    img: 'https://images.unsplash.com/photo-1616801060191-6085bad82f95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60',
-    title: 'Game',
-    description:"this is game"
-},
-{
+    img: "https://images.unsplash.com/photo-1616801060191-6085bad82f95?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDIwfHx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60",
+    title: "Game",
+    description: "this is game",
+    brand_img:
+      "https://www.virginmegastore.qa/medias/Unig-logo.webp?context=bWFzdGVyfHJvb3R8MTY1OHxhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW18aDMxL2hhNy8xMDI5OTcyNjY1OTYxNC9VbmlnLWxvZ28ud2VicHw5NWFkYmM0NTZlMTNlYjc4OTQ4ZTFkNDE3YTNmZjQyMTQ0ZDU0OWFjNDc2NWUxNDk4MjAxMjA4NTk2MzczZThl",
+      brand_name:"unsplace0"
+  },
+  {
     id: 3,
-    img: 'https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-    title: 'Water',
-    description:"this is water"
-},
-{
+    img: "https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    
+    title: "Water",
+    description: "this is water",
+    brand_img:
+      "https://www.virginmegastore.qa/medias/Sonos-logo.webp?context=bWFzdGVyfHJvb3R8MzIzNHxhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW18aDlmL2gzNi8xMDI5OTcyODk1MzM3NC9Tb25vcy1sb2dvLndlYnB8MzgxMzhmMGQxMDRiYTlkZGI4N2Y3NTJhM2MxM2IxMjMyZDE1YThhNmQxYWU4NDliZWU3ZTE4OGExMjFjNjY3Yw",
+      brand_name:"unsplace2"
+  },
+  {
     id: 4,
-    img: 'https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-    title: 'Water',
-    description:"this is water"
-},
-{
+    img: "https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    
+    title: "Water",
+    description: "this is water",
+    brand_img:
+      "https://www.virginmegastore.qa/medias/Mountain-logo.webp?context=bWFzdGVyfHJvb3R8Mzk1NHxhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW18aDI0L2gwYS8xMDI5OTczMTI3OTkwMi9Nb3VudGFpbi1sb2dvLndlYnB8MzBmYWVmYjEwMTY1Y2Y2MGI5N2U3NGQ0NGJhYjBmOWI1NGUxNTAyYTVjZjhjMWQ3NTZlODI1NjNiYjJmYjhiOQ",
+      brand_name:"unsplace1"
+  },
+  {
     id: 5,
-    img: 'https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
-    title: 'Water',
-    description:"this is water"
-},
-
-]
+    img: "https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    title: "Water",
+    description: "this is water",
+    brand_img:
+      "https://www.virginmegastore.qa/medias/Zwilling-logo.webp?context=bWFzdGVyfHJvb3R8MzY1MHxhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW18aDYwL2g1Yy8xMDI5OTczMDM2MjM5OC9ad2lsbGluZy1sb2dvLndlYnB8OGVlNDE1ZGEyMzVjMzAxMWY5Y2NiNjY5YTZmZGRlMGVkZjdiMjkyZDZiNjU5NWI2ZWFjYmNmNTE5ZTRiNjk0ZA",
+      brand_name:"unsplace5"
+  },
+  {
+    id: 6,
+    img: "https://images.unsplash.com/photo-1690660437195-a65346cd276a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+    title: "Water",
+    description: "this is water",
+    brand_img:
+      " https://www.virginmegastore.qa/medias/Dell-logo.webp?context=bWFzdGVyfHJvb3R8NDA2MnxhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW18aGE2L2gwMi8xMDI5OTczMTAxNzc1OC9EZWxsLWxvZ28ud2VicHwzNDg5NWVlM2Q5MGRjZjM3ZGI2N2IwODQwMjgxMzkzMDcyNTE0ZDI1ZmYwMWYxNDdhZWUxNWRkMTVhMTBlYjBk",
+      brand_name:"unsplace5"
+  },
+  {
+    id: 7,
+    img: "https://plus.unsplash.com/premium_photo-1674228288323-3bfbae70ad45?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+    title: "Books",
+    description: "this is book",
+    brand_img:
+      "https://www.virginmegastore.qa/medias/MiiR-logo.webp?context=bWFzdGVyfHJvb3R8MTU5OHxhcHBsaWNhdGlvbi9vY3RldC1zdHJlYW18aGEzL2g1NS8xMDI5OTczMDU1OTAwNi9NaWlSLWxvZ28ud2VicHxkYjQ5ZWQyMGRlMzQ0YmJlNDRjZDBmYzZkMGVjNDc3MTg1YTcxMjQ2MGMxMGVkYTExNWFkYzQ4NjZhZDBkOTky",
+      brand_name:"unsplace9"
+  },
+ 
+];
