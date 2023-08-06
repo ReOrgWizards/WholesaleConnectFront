eee0c0c533e1f51dbc402e39a041173690a52add
Laxmi <laxmibist009@gmail.com>
2023-08-06 23:14:43 +0545

    check in

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import "./App.scss";
-// import WCTitle from "./components/TItle/TItle";
-// import WCCategorySection from "./layouts/category/categorySection";
 import { categoryArr } from "./data";
 import WCHotDealsSection from "./layouts/hotDeals/hotdeals";
 function App() {
   return (
     <>
-      {/* <WCTitle title="Category Section" /> */}
-      {/* <WCCategorySection categoryArr={categoryArr} /> */}
-      <WCHotDealsSection categoryArr ={categoryArr }/>
-
+      <WCHotDealsSection categoryArr={categoryArr} />
     </>
   );
 }
