b8f237a9f54eca8438c7a200fe946dbe8ea249ad
Laxmi_Bist <98207003+LaxmiBist@users.noreply.github.com>
2023-08-06 22:09:04 +0545

    WCC-22-Redesign-Hot-deals-section (#15)
    * WCC-22-Redesign-Hot-deals-section
    
    * responsive check and desing update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.scss";
-import WCTitle from "./components/TItle/TItle";
-import WCCategorySection from "./layouts/category/categorySection";
+// import WCTitle from "./components/TItle/TItle";
+// import WCCategorySection from "./layouts/category/categorySection";
 import { categoryArr } from "./data";
+import WCHotDealsSection from "./layouts/hotDeals/hotdeals";
 function App() {
   return (
     <>
-      <WCTitle title="Category Section" />
-      <WCCategorySection categoryArr={categoryArr}/>
+      {/* <WCTitle title="Category Section" /> */}
+      {/* <WCCategorySection categoryArr={categoryArr} /> */}
+      <WCHotDealsSection categoryArr ={categoryArr }/>
+
     </>
   );
 }
