584bb39635ca47c71ede62ed526d5c42d1898b49
Laxmi <laxmibist009@gmail.com>
2023-08-06 18:40:23 +0545

    responsive check and desing update

--- src/components/cards/productCard/productCard.jsx
+++ src/components/cards/productCard/productCard.jsx
@@ -4,22 +4,28 @@ import { CartIcon, FavoriteIcon } from "../../../assets/Icons";
 const WCProductCard = ({ image, title, description, price }) => {
   return (
     <>
-      <div className="product__card__container">
-        <div className="product__card__img">
-          <img src={image} alt={title} />
+      <div className="productCard_container">
+        <div className="productCard_img_container">
+          <img src={image} alt="product" />
         </div>
-        <div className="product__card__content">
-          <p className="product__card__title ">{title}</p>
-          <p className="product__card__description">{description}</p>
-          <div className="product__card__content">
-            <div className="product__price__container">
-              <span>Rs. </span>
-              {price}
-            </div>
-            <div className="price__icon__container">
-              <CartIcon />
-              <FavoriteIcon />
-            </div>
+
+        <div className="row">
+          <p className="product_title">{title}</p>
+          <p className="product_description">{description}</p>
+          <div className="col-lg-6 col-6 col-md-6 col-sm-6">
+            <p className="product_price">
+              Rs. <span>1200/-</span>{" "}
+            </p>
+          </div>
+          <div className="col-lg-6 col-6 col-md-6 col-sm-6">
+            <p className="product_icon">
+              <span>
+                <i class="fa-solid fa-cart-shopping"></i>
+              </span>
+              <span>
+                <i class="fa-solid fa-heart"></i>
+              </span>
+            </p>
           </div>
         </div>
       </div>
