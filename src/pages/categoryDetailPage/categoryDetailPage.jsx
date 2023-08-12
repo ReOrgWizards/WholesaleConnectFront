6c6b8369808a81f0dac0e06b22f10d10920e6e6d
Laxmi <laxmibist009@gmail.com>
2023-08-12 16:11:42 +0545

    WCC-Redesing-Category-Page

---
+++ src/pages/categoryDetailPage/categoryDetailPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import WCTitle from '../../components/TItle/TItle'
+
+const WCCategoryDetailPage = () => {
+  return (
+   
+    <div className="container">
+        <WCTitle title="Category"/>
+    </div>
+  )
+}
+
+export default WCCategoryDetailPage
