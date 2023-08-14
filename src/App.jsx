d029cb4baeafeb15823079e521bf85de41e5f3b6
Laxmi <laxmibist009@gmail.com>
2023-08-14 05:58:21 +0545

    pull from development

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import "./App.scss";
 import WCFooter from "./components/footer/footer";
 import WCNavBarSection from "./layouts/navbar/navBar";
 import WCCategoryDetailPage from "./pages/categoryDetailPage/categoryDetailPage";
-import WCLandingPage from "./pages/landingpage/landingPage";
 
 function App() {
   return (
     <>
       <WCNavBarSection />
-      {/* <WCLandingPage/> */}
-      <WCCategoryDetailPage/>
-      <WCFooter/>
-     
+      <WCCategoryDetailPage />
+      <WCFooter />
     </>
   );
 }
