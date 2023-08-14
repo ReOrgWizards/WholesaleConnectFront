61b2d6653891aec1b4c1322327194c3dcab8e4d6
Laxmi <laxmibist009@gmail.com>
2023-08-14 05:54:36 +0545

    create value and their corresponding handle data function.

--- src/components/authentication/login/login.jsx
+++ src/components/authentication/login/login.jsx
@@ -1,32 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 const WCLogin = () => {
+  const [formData, setFormData] = useState({
+    emailPhone: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="container">
       <div className="login__content">
         <form>
           <div className="row">
             <div className="col-12 label__input__wrapper">
-              <label for="exampleInputEmail1" class="form-label">
+              <label htmlFor="emailPhone" className="form-label">
                 Email*/Phone no*
               </label>
               <input
-                type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Enter your email/phone no"
+                type="text"
+                className="form-control"
+                id="emailPhone"
+                name="emailPhone"
+                value={formData.emailPhone}
+                onChange={handleInputChange}
+                placeholder="Enter your email or phone no"
               />
             </div>
             <div className="col-12 label__input__wrapper">
-              <label for="exampleInputEmail1" class="form-label">
+              <label htmlFor="password" className="form-label">
                 Password*
               </label>
               <input
                 type="password"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                className="form-control"
+                id="password"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
                 placeholder="Enter your password"
               />
             </div>
@@ -35,10 +52,9 @@ const WCLogin = () => {
             <p>Forgot password?</p>
           </a>
           <div className="login__btn">
-          <button type="button">Log In</button>
-        </div>
+            <button type="submit">Log In</button>
+          </div>
         </form>
-        
       </div>
     </div>
   );
--- src/components/authentication/registration/registration.jsx
+++ src/components/authentication/registration/registration.jsx
@@ -1,92 +1,111 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
 
 const WCRegistration = () => {
+  const [formData, setFormData] = useState({
+    title: "Mrs./Ms.",
+    email: "",
+    firstName: "",
+    lastName: "",
+    countryCode: "+ 977",
+    phoneNumber: "",
+    password: "",
+  });
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div className="container">
       <div className="registration__contant">
         <form>
           <div className="row">
             <div className="col-12 reg__lbl__input">
-              <label for="exampleInputEmail1" class="form-label">
-                Title*
-              </label>
-              <select id="disabledSelect" class="form-select">
+              <label className="form-label">Title*</label>
+              <select
+                name="title"
+                className="form-select"
+                value={formData.title}
+                onChange={handleInputChange}
+              >
                 <option>Mrs./Ms.</option>
                 <option>Mr.</option>
               </select>
             </div>
-
             <div className="col-12 reg__lbl__input">
-              <label for="exampleInputEmail1" class="form-label">
-                Email Address*
-              </label>
+              <label className="form-label">Email Address*</label>
               <input
                 type="email"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                className="form-control"
+                name="email"
+                value={formData.email}
+                onChange={handleInputChange}
                 placeholder="Enter your email"
               />
             </div>
             <div className="col-12 reg__lbl__input">
-              <label for="exampleInputEmail1" class="form-label">
-                First Name*
-              </label>
+              <label className="form-label">First Name*</label>
               <input
                 type="text"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                className="form-control"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleInputChange}
                 placeholder="Enter your first name"
               />
             </div>
             <div className="col-12 reg__lbl__input">
-              <label for="exampleInputEmail1" className="form-label">
-                Last Name*
-              </label>
+              <label className="form-label">Last Name*</label>
               <input
                 type="text"
                 className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleInputChange}
                 placeholder="Enter your last name"
               />
             </div>
-            <div class="col-12 reg__lbl__input">
-              <label for="exampleInputEmail1" class="form-label">
-                Title*
-              </label>
-              <div className="input-group number__dropdown">
-              <span className="input-group-text" id="basic-addon1">
-                <select id="disabledSelect" class="form-select">
-                  <option>+ 977</option>
-                  <option>+ 908</option>
-                </select>
-              </span>
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter your phone.no"
-                aria-label="Username"
-                aria-describedby="basic-addon1"
-              />
-            </div>
-            </div>
-
             <div className="col-12 reg__lbl__input">
-              <label for="exampleInputEmail1" class="form-label">
-                Password*
-              </label>
+              <label className="form-label">Phone Number*</label>
+              <div className="input-group number__dropdown">
+                <span className="input-group-text">
+                  <select
+                    name="countryCode"
+                    className="form-select"
+                    value={formData.countryCode}
+                    onChange={handleInputChange}
+                  >
+                    <option>+ 977</option>
+                    <option>+ 908</option>
+                  </select>
+                </span>
+                <input
+                  type="text"
+                  className="form-control"
+                  name="phoneNumber"
+                  value={formData.phoneNumber}
+                  onChange={handleInputChange}
+                  placeholder="Enter your phone number"
+                />
+              </div>
+            </div>
+            <div className="col-12 reg__lbl__input">
+              <label className="form-label">Password*</label>
               <input
                 type="password"
-                class="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
+                className="form-control"
+                name="password"
+                value={formData.password}
+                onChange={handleInputChange}
                 placeholder="Enter your password"
               />
             </div>
-            <p className="pass__word__criteria ">
+            <p className="pass__word__criteria">
               Your password must contain at least 8 characters, including one
               uppercase, one lowercase, one number, and one special character.
             </p>
@@ -95,7 +114,7 @@ const WCRegistration = () => {
               <Link to="#"> Terms </Link> &<Link to="#"> Privacy Policy.</Link>
             </p>
             <div className="registration__btn">
-              <button>Registration</button>
+              <button type="submit">Registration</button>
             </div>
           </div>
         </form>
