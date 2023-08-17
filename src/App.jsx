19c30201d8a6baab2b4c1e48b75825b8ff4fc290
Hypekage <jmestmnag@gmail.com>
2023-08-17 20:55:22 +0545

    `Component` managed.

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import "./App.scss";
 import WCFooter from "./components/footer/footer";
 import WCNavBarSection from "./layouts/navbar/navBar";
 import WCRoutes from "./routes/Routes";
-import WCProductDetailPage from "./pages/productDetailPage/productDetailPage";
 
 function App() {
   return (
     <>
       <WCNavBarSection />
-      <WCRoutes/>
+      <WCRoutes />
       <WCFooter />
-      <WCProductDetailPage/>
-      <WCFooter/>
     </>
   );
 }
